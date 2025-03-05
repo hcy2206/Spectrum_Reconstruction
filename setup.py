a72b088f2c6df63865780ef20a4b8025de91477f
hcy2206 <97283537+hcy2206@users.noreply.github.com>
2025-03-05 23:08:37 +0800

    Update setup.py: bump version to 0.2 and refine package dependencies for compatibility

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='SpectrumReconstruction',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
-        'numpy',
-        'scipy',
-        'matplotlib',
-        'pandas'
+        'numpy>=2.0.1, <3.0.0',
+        'scikit-learn>=1.5.2, <2.0.0',
+        'pandas>=2.2.3, <3.0.0',
+        'plotly>= 5.24.1, <7.0.0',
+        'numba>=0.61.0, <1.0.0'
     ],
     author='hcy2206',
     author_email='97283537+hcy2206@users.noreply.github.com.',
@@ -36,6 +37,6 @@ setup(
     ''',
     license='MIT',
     keywords='spectrum reconstruction photodetector simulation',
-    python_requires='>=3.12',
+    python_requires='>=3.10',
     url='https://github.com/hcy2206/SpectrumReconstruction'
 )
